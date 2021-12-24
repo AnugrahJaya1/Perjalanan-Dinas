d999b221f8c3cef8af7653c88f150cf2a2da6642
anugrahjaya1 <anugrahjaya23@gmail.com>
2021-12-24 20:42:10 +0700

    add LoginController and adjust router for login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
-})->name('login');
+Route::group(
+    [
+        'middleware' => 'guest',
+        'namespace' => 'App\Http\Controllers',
+    ],function($router){
+        Route::get('/', [LoginController::class, 'index'])->name('login');
+    }
+);
