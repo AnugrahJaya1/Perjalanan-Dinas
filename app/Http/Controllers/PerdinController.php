2f561d2aba7c625bee26af94da9d998f7242484e
anugrahjaya1 <anugrahjaya23@gmail.com>
2021-12-24 22:45:24 +0700

    add create and store (not done) function

--- app/Http/Controllers/PerdinController.php
+++ app/Http/Controllers/PerdinController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class PerdinController extends Controller
 {
@@ -14,5 +15,29 @@ class PerdinController extends Controller
     public function index()
     {
         return view('perdins.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $response = Http::get('http://akhdani.net:12345/api/lokasi/list')->json();
+        
+        return view('perdins.create', compact('response'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        return redirect()->route('perdins.index')->with('message', 'Perdin Registered Succesfully');
     }
 }
