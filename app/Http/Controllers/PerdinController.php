4f68b50051447b869d477e624dcc3d55d2fd9c0f
anugrahjaya1 <anugrahjaya23@gmail.com>
2021-12-25 02:13:05 +0700

    create function store for save data into database

--- app/Http/Controllers/PerdinController.php
+++ app/Http/Controllers/PerdinController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Perdin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Http;
 
 class PerdinController extends Controller
@@ -14,7 +16,12 @@ class PerdinController extends Controller
      */
     public function index()
     {
-        return view('perdins.index');
+        // dd(Cookie::get('username'));
+        $pegawai = Http::get('http://akhdani.net:12345/api/pegawai/username/' . Cookie::get('username'))->json();
+
+        $perdins = Perdin::where('id_pegawai', $pegawai['pegawaiid'])->get();
+        
+        return response(view('perdins.index', compact('perdins')))->cookie('username', $pegawai['username'], 60);
     }
 
     /**
@@ -25,7 +32,7 @@ class PerdinController extends Controller
     public function create()
     {
         $response = Http::get('http://akhdani.net:12345/api/lokasi/list')->json();
-        
+
         return view('perdins.create', compact('response'));
     }
 
@@ -37,7 +44,38 @@ class PerdinController extends Controller
      */
     public function store(Request $request)
     {
-        
-        return redirect()->route('perdins.index')->with('message', 'Perdin Registered Succesfully');
+        $data = $request->validate([
+            'alasan_perdin' => ['required', 'string'],
+            'tanggal_berangkat' => ['required', 'date'],
+            'tanggal_pulang' => ['required', 'date'],
+            'id_lokasi_tujuan' => ['required', 'integer']
+        ]);
+
+        // dd($data);
+
+        $durasi = date_diff(date_create($data['tanggal_berangkat']), date_create($data['tanggal_pulang']));
+        $durasi = substr($durasi->format("%R%a"), 1);
+
+        $uang_saku = 100000;
+
+        $pegawai = Http::get('http://akhdani.net:12345/api/pegawai/username/' . Cookie::get('username'))->json();
+        $id_peangawai = $pegawai['pegawaiid'];
+
+        $id_lokasi_awal = 345; // id kota bandung (default)
+
+        Perdin::create([
+            'alasan_perdin' => $request['alasan_perdin'],
+            'tanggal_berangkat' => $request['tanggal_berangkat'],
+            'tanggal_pulang' => $request['tanggal_pulang'],
+            'durasi' => $durasi*1,
+            'uang_saku' => $uang_saku,
+            'id_pegawai' => $id_peangawai*1,
+            'id_lokasi_awal' => $id_lokasi_awal,
+            'id_lokasi_tujuan' => $request['id_lokasi_tujuan']*1,
+            'id_approval' => null,
+            'status' => 0
+        ]);
+
+        return redirect()->route('perdins.index')->with('message', 'Perdin Registered Succesfully')->cookie('username', $pegawai['username'], 60);
     }
 }
