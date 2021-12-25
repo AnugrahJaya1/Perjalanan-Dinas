f34d609ec619fb5b7b41523a9ec404ef23803289
anugrahjaya1 <anugrahjaya23@gmail.com>
2021-12-25 22:05:49 +0700

    adjust route because not use middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\PerdinController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,14 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(
-    [
-        'middleware' => 'guest',
-        'namespace' => 'App\Http\Controllers',
-    ],function($router){
-        Route::get('/', [LoginController::class, 'index'])->name('login');
-        Route::post('/login', [LoginController::class, 'authenticate']);
-    }
-);
-
+// Route::group(
+//     [
+//         'middleware' => 'guest',
+//         'namespace' => 'App\Http\Controllers',
+//     ],function($router){
+        // Route::get('/', [LoginController::class, 'index'])->name('login');
+        // Route::post('/login', [LoginController::class, 'authenticate']);
+//     }
+// );
+Route::get('/', [AuthController::class, 'index'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 Route::resource('/perdins', PerdinController::class);
